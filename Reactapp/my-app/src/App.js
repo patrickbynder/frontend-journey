32062b9666f49d4238d6d029e1c7877b9879cdd4
patrickbynder <patrick.bakker@bynder.com>
2022-03-07 16:17:01 +0100

    new react hooks

--- Reactapp/my-app/src/App.js
+++ Reactapp/my-app/src/App.js
@@ -1,25 +1,29 @@
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const Header = () => {
+    const [currentPage, setCurrentPage] = useState('Home');
+
+    const currentPageText = `Your current page is: ${currentPage}`;
+
+    return (
+        <>
+            <div>{currentPageText}</div>
+
+            <button onClick={() => setCurrentPage('bynderpage')}>
+                Click me
+            </button>
+        </>
+    );
+};
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Header />
+        </div>
+    );
 }
 
 export default App;
